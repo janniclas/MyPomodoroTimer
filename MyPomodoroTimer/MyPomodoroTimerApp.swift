8106f1af5211c2b6a92e690ce3b877a42fe68764
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-10-31 12:34:12 +0100

    Initial Commit

---
+++ MyPomodoroTimer/MyPomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPomodoroTimerApp.swift
+//  MyPomodoroTimer
+//
+//  Created by Jan-Niclas Strüwer on 31.10.22.
+//
+
+import SwiftUI
+
+@main
+struct MyPomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
