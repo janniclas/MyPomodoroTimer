144c9602755f8f563b18c0b19f1011f3e068217f
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-11-03 15:24:52 +0100

    fixed menu bar

--- MyPomodoroTimer/MyPomodoroTimerApp.swift
+++ MyPomodoroTimer/MyPomodoroTimerApp.swift
@@ -7,21 +7,38 @@
 
 import SwiftUI
 
+@available(macOS 13.0, *)
 @main
 struct MyPomodoroTimerApp: App {
+    
+    @AppStorage("showMenuBarExtra") private var showMenuBarExtra = true
+    @State private var icon = "10:00"
+    init() {
+        if #unavailable(macOS 13.0) {
+            self.showMenuBarExtra = false
+        }
+    }
     var body: some Scene {
+
         WindowGroup {
             ContentView()
         }
-        #if os(macOS)
+#if os(macOS)
         Settings {
             SettingsView()
         }
-        #endif
+
+            MenuBarExtra(
+                icon,
+                isInserted: $showMenuBarExtra) {
+                    let t = TimerView(name: "Menu Bar", startTime: 600)
+                        t
+                        .onReceive(t.timer.$time) { newValue in
+                            self.icon = newValue
+                        }
+                }
+                .menuBarExtraStyle(.window)
+ 
+#endif
     }
 }
-
-
-// TODO:
-// menu bar extras
-// -- popovers
